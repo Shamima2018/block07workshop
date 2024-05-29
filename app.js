e758896ad354b1205ea58ad2f1966c0c450e156f
Shamima2018 <shamima.k.sultana@gmail.com>
2024-05-28 20:42:23 -0400

    block07

---
+++ app.js
@@ -0,0 +1,16 @@
+let numOfCows = 50;
+console.log(numOfCows);
+
+console.log('Hello World');
+
+console.log("Hello" + " " + "Disney World");
+
+let numOfUmbrellas = 10 * 3;
+let num = 5 + 1;
+console.log(num);
+concat("hello" + " " + "World.");
+
+let str1 = "Hello,";
+let str2 = "I am";
+let name = "Josh";
+alert(str1 + " " + str2 + " " + name);
