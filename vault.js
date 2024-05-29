703b36151b7e10c26abdcdde14317f2d03a90029
Shamima2018 <shamima.k.sultana@gmail.com>
2024-05-28 21:06:42 -0400

    block07

---
+++ vault.js
@@ -0,0 +1,9 @@
+
+let str = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//defining variables 
+let var1 = 5*2;
+let var2 = 20+20;
+let var3 = 40-1;
+console.log (str + var1 + "-" + var2 + "-" + var3);
+alert (var1 + "-" + var2 + "-" + var3);
+
